f6d66901bbe30050abbda148343b8f01ca562f7b
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-06-06 09:40:38 -0300

    Luz da sala de jantar funcionando

--- README.md
+++ README.md
@@ -7,3 +7,20 @@ Trabalho do Laboratório de Sistemas Embutidos Microprocessados
 
 - Roteiro: [Desafio2.pdf](./Desafio2.pdf)
 - Montagem de apoio: https://www.tinkercad.com/things/jrfsLzzrpUm
+
+
+
+
+
+## Autores
+
+* **Arthur Siqueira** - *Graduando, Eng. Controle e Automação - PUC Minas*
+* **Guilherme Rodrigues** - *Graduando, Eng. Controle e Automação - PUC Minas* - [GuilhermeRodrigues680](https://github.com/guilhermerodrigues680)
+
+## Reconhecimentos
+
+- http://aebenficaonline.blogspot.com/2019/01/o-que-e-o-codigo-ascii.html
+- https://sites.google.com/site/qeewiki/books/avr-guide/pwm-on-the-atmega328
+- https://www.engineersgarage.com/knowledge_share/generating-variable-frequency-with-arduino/
+- http://ww1.microchip.com/downloads/en/DeviceDoc/ATmega48A-PA-88A-PA-168A-PA-328-P-DS-DS40002061A.pdf
+- https://www.tinkercad.com/users/kgxyRhQrjsG-prof-hegler-rocha?category=circuits&sort=latest&view_mode=default
--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,18 @@ LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
 #define OFF       0
 #define ON        1
 
+#define TIMER1_MODULE 0xFFFF
+
+// Delay no tinkercad em ms, usado em loops para aumentar a fluidez da simulacao
+#define DELAY_SIMULACAO_TINKERCAD 10
+#define _DELAY_TINKERCAD() (_delay_ms(DELAY_SIMULACAO_TINKERCAD))
+// #define _DELAY_TINKERCAD()
+
+#define _ltrans(x, xi, xm, yi, ym) (long)((long)(x - xi) * (long)(ym - yi)) / (long)(xm - xi) + yi
+
 void quarto1(void);
 void quarto2(void);
+void salajantar(void);
 /*** TECLADO*/
 char LeTecla(void);
 /*******    PARA USO DO DISPLAY    ***********************/
@@ -31,20 +41,49 @@ void putmessage(int l, int c, char *msg);
 void putnumber_i(int l, int c, long ni, int nd);
 void putnumber_f(int l, int c, float ni, int nd);
 
-int main(void)
+
+// Colocada a funcao 'void setup' pois o tinkerkad dá bug ao usar o TIMER1 sem ela
+void setup()
+// int main(void)
 {
   char tecla;
+  unsigned int result=0;
   init_dsp(2, 16);
   putmessage(0, 0, "  Smart  Light  ");
   
-  DDRB = _BV(Q2) | _BV(Q1); // Configura como saida Q1 e Q2
   
+
+  // ICR1 = 0xFFFF;                           // Seta o TOP do timer1 de 16bit
+  // OCR1A = 0x3FFF;                          // set PWM for 25% duty cycle @ 16bit
+  // OCR1B = 0xBFFF;                          // set PWM for 75% duty cycle @ 16bit
+  // TCCR1A |= (1 << COM1A1) | (1 << COM1B1); // set none-inverting mode
+
+  // TCCR1A |= (1 << WGM11); // set Fast PWM mode using ICR1 as TOP
+  // TCCR1B |= (1 << WGM12) | (1 << WGM13);
+
+  // TCCR1B |= (1 << CS10); // START the timer with no prescaler
+
+  // // Configura como saida Q1 e Q2, e tambem os pinos OC1A(PB1) e OC1B(PB2)
+  // DDRB = _BV(Q2) | _BV(PORTB2) | _BV(PORTB1) | _BV(Q1);
+
+  ICR1    = TIMER1_MODULE-1; // define o periodo do PWM 4.096ms ((1/16e6)*1*65536 * 1000) 
+  TCCR1A  = _BV(COM1A1) | _BV(WGM11);// PWM em OC1A(PB1) modo fast PWM com OCR1A
+  // TCCR1B  = _BV(WGM13) | _BV(WGM12) | _BV(CS11); // FPS de 8
+  TCCR1B  = _BV(WGM13) | _BV(WGM12) | _BV(CS10); // FPS de 1
+	// DDRB    = _BV(DDB1);  // Configura OC1B como saida
+  DDRB = _BV(Q2) | _BV(DDB1) | _BV(Q1); // Configura como saida Q1 e Q2, e tambem os pinos OC1A(DDB1)
+  // result = _ltrans(100, 0, 100, 0, TIMER1_MODULE-1);
+  // OCR1A = result;
+  // OCR1A = (int)(_ltrans(50, 0, 100, 0, TIMER1_MODULE-1));
+  OCR1A = 0;
+
+
   for (;;)
   {
     tecla = LeTecla();
     if (tecla != 0)
     {
-      putnumber_i(1,0, tecla, 3);
+      putnumber_i(1, 0, tecla, 3);
       switch (tecla)
       {
       case QUARTO_1:
@@ -57,14 +96,14 @@ int main(void)
         /* code */
         break;
       case SALA_JTR:
-        /* code */
+        salajantar();
         break;
       default: // As teclas nao configuradas passam por aqui
         break;
       }
     }
 
-    _delay_ms(10);
+    _DELAY_TINKERCAD(); // Delay para melhorar a fluidez na simulacao do tinkercad
   }
 }
 
@@ -75,6 +114,7 @@ void quarto1(void) {
   putmessage(1,0, "Press ON ou OFF");
   while (tecla == 0) { // Fica preso aqui até apertar uma tecla
     tecla = LeTecla();
+    _DELAY_TINKERCAD(); // Delay para melhorar a fluidez na simulacao do tinkercad
   }
   if (tecla == LIGA)
   {
@@ -88,7 +128,12 @@ void quarto1(void) {
 
   // Limpa o display
   lcd.clear();
+  putmessage(0, 0, "  Smart  Light  ");
 }
+
+
+// Colocada a funcao 'void loop' pois o tinkerkad dá bug ao usar o TIMER1 sem ela
+void loop(){}
 
 
 // ==> Rotina do QUARTO 2
@@ -97,6 +142,7 @@ void quarto2(void) {
   putmessage(1,0, "Press ON ou OFF");
   while (tecla == 0) { // Fica preso aqui até apertar uma tecla
     tecla = LeTecla();
+    _DELAY_TINKERCAD(); // Delay para melhorar a fluidez na simulacao do tinkercad
   }
   if (tecla == LIGA)
   {
@@ -110,6 +156,47 @@ void quarto2(void) {
 
   // Limpa o display
   lcd.clear();
+  putmessage(0, 0, "  Smart  Light  ");
+}
+
+
+void salajantar(void)
+{
+  unsigned int valor;
+  char tecla = LeTecla();
+  
+  putmessage(1,0, "Pres. ON,OFF,0a9");
+
+  while (tecla == 0) { // Fica preso aqui até apertar uma tecla
+    tecla = LeTecla();
+    _DELAY_TINKERCAD(); // Delay para melhorar a fluidez na simulacao do tinkercad
+  }
+
+  if (tecla >= '0' && tecla <= '9') // Se é um numero
+  {
+    // Converte o caracter em valor numerico de 0 a 9 em seguida de 0 a 100 %
+    valor = _ltrans(tecla - '0', 0, 10, 0, TIMER1_MODULE-1);
+    OCR1A = valor;
+  } 
+  else if (tecla == LIGA)
+  {
+    valor = _ltrans(10, 0, 10, 0, TIMER1_MODULE-1);
+    OCR1A = valor;
+  }
+  else if (tecla == DESLIGA)
+  {
+    valor = _ltrans(0, 0, 10, 0, TIMER1_MODULE-1);
+    OCR1A = valor;
+  }
+  else
+  {
+    // tecla diferente apertada
+    // Nao faz nada
+  }
+
+  // Limpa o display
+  lcd.clear();
+  putmessage(0, 0, "  Smart  Light  ");
 }
 
 
